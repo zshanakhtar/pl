b420367bd069b848ae4ec324ba75c02047e4c1ed
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-16 16:11:07 +0530

    removed unnecessary mailer

--- php-back/functions.php
+++ php-back/functions.php
@@ -55,27 +55,5 @@ function generateaccesscode($access){
     $code=bin2hex($bytes);
     return $access.$code;
 }
-function sendmail($to,$subject,$body){
-    $mail = new PHPMailer(); // create a new object
-    $mail->IsSMTP(); // enable SMTP
-    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "authenticatorkaprakop";
-    $mail->Password = "deleted";
-    $mail->SetFrom("deleted");
-    $mail->Subject = $subject;
-    $mail->Body = $body;
-    $mail->AddAddress($to);
 
-    if(!$mail->Send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    }
-    else {
-        echo "Message has been sent";
-    }
-}
 ?>
