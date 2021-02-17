2fdb7faecc82d68e399801171c14b31e954aa674
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-18 02:04:47 +0530

    navbar with logout button

---
+++ php-front/nav/logout.php
@@ -0,0 +1,23 @@
+
+<!-- Navbar -->
+<nav class="z-navbar col-xs-12">
+    <div class="col-xs-1">
+        <img class="col-xs-12" src="img/logo.png" alt="WEBmis_logo">
+        <hr class="col-xs-12" style="opacity:0;"/>
+        <a href="" target="_blank"><img src="img/linkedin.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/facebook.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/youtube.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a>
+    </div>
+    <div class="col-xs-10">
+        <div class="text-center">
+        <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+    <div class="col-xs-1">
+        <div class="col-xs-12">
+            <a href="php-back/logout.php"><img src="img/logo.png" height="80px" class="logout-button"></a>
+        </div>
+    </div>
+</nav>
