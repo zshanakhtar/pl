545dd4e6909a6e2e9dca1b566936d46a5d82fd61
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-18 00:00:00 +0530

    commented out create user validation

--- php-back/f_registration_validate.php
+++ php-back/f_registration_validate.php
@@ -2,10 +2,10 @@
 function validate($data)
 {
 extract($_POST);
-if( !inputvalidate('exact-length',array($username=>10)) )
-    return 'University roll incorrect length, please input 10 characters.';
-if( !inputvalidate('min-length',array($pass=>6)) )
-    return 'Password is of incorrect length, please input atleast 6 characters.';
+// if( !inputvalidate('exact-length',array($username=>10)) )
+//     return 'University roll incorrect length, please input 10 characters.';
+// if( !inputvalidate('min-length',array($pass=>6)) )
+//     return 'Password is of incorrect length, please input atleast 6 characters.';
 return 'success';
 }
 ?>
