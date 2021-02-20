70ffdfc3c5d38ffa4e85cfe4581eee85732489f0
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-20 16:26:36 +0530

    updated nav removed noise

--- php-front/nav/login.php
+++ php-front/nav/login.php
@@ -2,17 +2,17 @@
 <!-- Navbar -->
 <nav class="z-navbar col-sm-12">
     <div class="col-sm-1">
-        <img class="col-sm-12" src="img/logo.png" alt="KEC_logo">
+        <!-- <img class="col-sm-12" src="img/logo.png" alt="KEC_logo">
         <hr class="col-sm-12" style="opacity:0;"></hr>
         <a href="" target="_blank"><img src="img/linkedin.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/facebook.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/youtube.png" height="30px"></a>
-        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a> -->
     </div>
     <div class="col-sm-10">
         <div class="text-center">
-            <h1>Lorem ipsum</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <h1>Information Retrieval System</h1>
+            <p>Create, Retrieve and modify headlines </p>
         </div>
     </div>
 </nav>
--- php-front/nav/logout.php
+++ php-front/nav/logout.php
@@ -2,22 +2,22 @@
 <!-- Navbar -->
 <nav class="z-navbar col-xs-12">
     <div class="col-xs-1">
-        <img class="col-xs-12" src="img/logo.png" alt="WEBmis_logo">
+        <!-- <img class="col-xs-12" src="img/logo.png" alt="WEBmis_logo">
         <hr class="col-xs-12" style="opacity:0;"/>
         <a href="" target="_blank"><img src="img/linkedin.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/facebook.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/youtube.png" height="30px"></a>
-        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a> -->
     </div>
     <div class="col-xs-10">
         <div class="text-center">
-        <h1>Lorem ipsum</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        <h1>Information Retrieval System</h1>
+        <p>Create, Retrieve and modify headlines </p>
         </div>
     </div>
     <div class="col-xs-1">
-        <div class="col-xs-12">
+        <!-- <div class="col-xs-12">
             <a href="php-back/logout.php"><img src="img/logo.png" height="80px" class="logout-button"></a>
-        </div>
+        </div> -->
     </div>
 </nav>
