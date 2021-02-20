2bd13874af68615cc428c02a573cedf590f697f2
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-20 16:47:34 +0530

    minor

--- php-front/nav/logout.php
+++ php-front/nav/logout.php
@@ -2,12 +2,12 @@
 <!-- Navbar -->
 <nav class="z-navbar col-xs-12">
     <div class="col-xs-1">
-        <img class="col-xs-12" src="img/logo.png" alt="WEBmis_logo">
+        <!-- <img class="col-xs-12" src="img/logo.png" alt="WEBmis_logo">
         <hr class="col-xs-12" style="opacity:0;"/>
         <a href="" target="_blank"><img src="img/linkedin.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/facebook.png" height="30px"></a>
         <a href="" target="_blank"><img src="img/youtube.png" height="30px"></a>
-        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a>
+        <a href="" target="_blank"><img src="img/twitter.png" height="30px"></a> -->
     </div>
     <div class="col-xs-10">
         <div class="text-center">
@@ -16,8 +16,8 @@
         </div>
     </div>
     <div class="col-xs-1">
-        <div class="col-xs-12">
+        <!-- <div class="col-xs-12">
             <a href="php-back/logout.php"><img src="img/logout.png" height="80px" class="logout-button"></a>
-        </div>
+        </div> -->
     </div>
 </nav>
