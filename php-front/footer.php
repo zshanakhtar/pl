7c58eaf4b936c57d1ad12ae7377193f5c2fd89ee
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-02-20 16:27:25 +0530

    updated footer

--- php-front/footer.php
+++ php-front/footer.php
@@ -1,23 +1,51 @@
 <footer class="col-xs-12 z-footer">
     <div class="col-xs-12 pull-right">
       <span class="col-xs-8">
-        <br>Information Retrieval System
+        <br>
+        <br>
+        Information Retrieval System
         <br>This is for demonstration purposes only
-        <br>Developed by
       </span>
-      <span class="col-xs-2">
-        <span class="glyphicon glyphicon-phone"></span> Adesh Kumar Singh
-        <br><span class="glyphicon glyphicon-phone"></span> Kushagra Agrawal
-        <br><span class="glyphicon glyphicon-phone"></span> Rahul Yadav
-        <br><span class="glyphicon glyphicon-phone"></span> Soma Vikas Reddy
-        <br><span class="glyphicon glyphicon-phone"></span> Zeeshan Akhtar
+      <span class="col-xs-1">
+        <br>
+        <br>
+        Developed by
       </span>
-      <span class="col-xs-2">
-        <span class="glyphicon glyphicon-envelope"></span> 2020CS02
-        <br><span class="glyphicon glyphicon-envelope"></span> 2020CS06
-        <br><span class="glyphicon glyphicon-envelope"></span> 2020IS18
-        <br><span class="glyphicon glyphicon-envelope"></span> 2020IS22
-        <br><span class="glyphicon glyphicon-envelope"></span> 2020SW23
-      </span> 
+      <span class="col-xs-3">
+        <span class="pull-left">
+            <span class="glyphicon glyphicon-user"></span> Adesh Kumar Singh
+        </span>
+        <span class="pull-right">
+            <span class="glyphicon glyphicon-info-sign"></span> 2020CS02
+        </span>
+        <br>
+        <span class="pull-left">
+            <span class="glyphicon glyphicon-user"></span> Kushagra Agrawal
+        </span>
+        <span class="pull-right">
+            <span class="glyphicon glyphicon-info-sign"></span> 2020CS06
+        </span>
+        <br>
+        <span class="pull-left">
+            <span class="glyphicon glyphicon-user"></span> Rahul Yadav
+        </span>
+        <span class="pull-right">
+            <span class="glyphicon glyphicon-info-sign"></span> 2020IS18
+        </span>
+        <br>
+        <span class="pull-left">
+            <span class="glyphicon glyphicon-user"></span> Soma Vikas Reddy
+        </span>
+        <span class="pull-right">
+            <span class="glyphicon glyphicon-info-sign"></span> 2020IS22
+        </span>
+        <br>
+        <span class="pull-left">
+            <span class="glyphicon glyphicon-user"></span> Zeeshan Akhtar
+        </span>
+        <span class="pull-right">
+            <span class="glyphicon glyphicon-info-sign"></span> 2020SW23
+        </span>
+      </span>
   </div>
 </footer>
