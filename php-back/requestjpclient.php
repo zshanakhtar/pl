3711294633732d271479afe38057ce97b49e58f9
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-03-31 14:53:47 +0530

    jeopardy client request backend

---
+++ php-back/requestjpclient.php
@@ -0,0 +1,34 @@
+<?php
+include "../php-back/".'connection.php'; //connect the connection page
+include 'functions.php';
+
+session_start();
+$error_main="Game Creation Failed";
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+	
+    extract($_POST);
+
+    $username = mysqli_real_escape_string($conn, $username);//protection of sql injection replaces special characters with respective escape sequences
+
+    if($gamecode!="")
+    {
+    	$gamecode = mysqli_real_escape_string($conn, $gamecode);
+        $_SESSION['username']=$username;
+    	$_SESSION['gamecode']=$gamecode;
+    }
+    else
+    {
+    	$gamecode = generategamecode();
+        $_SESSION['username']=$username;
+        $_SESSION['gamecode']=$gamecode;
+        $_SESSION['newgame']=true;
+    }
+    echo '<script>window.location="jpclient.php"</script>';
+    //successdisplay("Game created successfully","username:$username,gamecode:$gamecode");
+}
+else{
+    errordisplay($error_main,'');
+}
+
+mysqli_close($conn); // Closing Connection with Database
+?>
